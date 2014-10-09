1efde65211f5cbb89a7e83125646ae5324b1b134
Pavel Malko <pavel.malko@kharkiv.kyivstar.ua>
2014-10-09 20:58:55 +0300

    2014.10.09

--- Electronic Station/vigenere-cipher.py
+++ Electronic Station/vigenere-cipher.py
@@ -1,6 +1,14 @@
 __author__ = 'Pavel.Malko'
 def decode_vigenere(old_decrypted, old_encrypted, new_encrypted):
-    return "???"
+    key = [chr((ord(old_encrypted[char]) - ord(old_decrypted[char]) + 26) % 26 + ord('A')) for char in range(len(old_encrypted))]
+    keylength = len(key)
+    for i in range(1, len(key)):
+        for j in range(i, len(key)):
+            if key[j] != key[j%i]: break
+        else:
+            keylength = i
+            break
+    return "".join([chr((ord(new_encrypted[char]) - ord(key[char % keylength]) + 26) % 26 + ord('A')) for char in range(len(new_encrypted))])
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -11,3 +19,9 @@ if __name__ == '__main__':
     assert decode_vigenere('LOREMIPSUM',
                            'OCCSDQJEXA',
                            'OCCSDQJEXA') == "LOREMIPSUM", "DOLORIUM"
+
+    assert decode_vigenere("ANDNOWFORSOMETHINGCOMPLETELYDIFFERENT",
+                           "PLWUCJUMKZCZTRAPBTRMFWZRICEFRVUDXYSAI",
+                           "XKTSIZQCKQOPZYGKWZDIBZZRTNTSZAXEAAOASGPVFXPJEKOLXANARBLLMYSRHGLRWCPLWQIZEGEPYRIMIYSFHUBSRSAMPLFFXNNACALMFLBFRJHAVVCETURUPLZHFBJLWPBOPPL") \
+                        == "IMALUMBERJACKANDIMOKISLEEPALLNIGHTANDIWO" \
+                           "RKALLDAYICUTDOWNTREESISKIPANDJUMPILIKETOPRESSWILDFLOWERSIPUTONWOMENSCLOTHINGANDHANGAROUNDINBARS", 'NotOk'
